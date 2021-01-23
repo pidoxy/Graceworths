8ab13faa34a0b86aff083a564b5fb4b9eb7f0ee1
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-01-23 19:53:03 +0100

    update

---
+++ graceworths/src/components/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AboutPage = ({ bgColor }) => {
+  return <div className={` ${bgColor} about-page`}>About Page</div>;
+};
+
+export default AboutPage;
---
+++ graceworths/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = ({ bgColor }) => {
+  return (
+    <div className={` ${bgColor} navbar`}>
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
